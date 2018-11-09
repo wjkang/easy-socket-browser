ecbb8cc4f49c0f57629bc0bf00b8b08dc6941807
wjkang <977865769@qq.com>
2018-11-09 12:02:11 +0800

    使用rollup打包

---
+++ config/rollup.config.aio.js
@@ -0,0 +1,30 @@
+// rollup.config.js
+
+var babel = require("rollup-plugin-babel");
+var nodeResolve = require('rollup-plugin-node-resolve');
+var commonjs = require('rollup-plugin-commonjs');
+var common = require('./rollup.js');
+
+export default {
+    input: 'src/index.js',
+    output: {
+        file: 'lib/easy-socket.js',
+        format: 'umd',
+        // 如果不同时使用 export 与 export default 可打开legacy
+        legacy: true,
+        name: 'EasySocket',
+        banner: common.banner,
+    },
+    plugins: [
+        nodeResolve({
+            main: true
+        }),
+        commonjs({
+            include: 'node_modules/**',
+        }),
+        babel({
+            runtimeHelpers: true,
+            exclude: 'node_modules/**'
+        })
+    ]
+};
